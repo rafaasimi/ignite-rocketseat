9eb3f8831844e7e8936302b4d8995ce5a560a438
Rafael Zorzenon <rafaasimi@hotmail.com>
2021-07-24 10:15:00 -0300

    Created a open modal function

--- Chapter 2/dtmoney/src/App.tsx
+++ Chapter 2/dtmoney/src/App.tsx
@@ -1,13 +1,34 @@
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
 import { GlobalStyle } from "./styles/global";
+import { useState } from "react";
+
+
 
 export function App() {
+  const [isNewTransctionModalOpen, setIsNewTransctionModalOpen] =
+    useState(false);
+
+  function handleOpenNewTransctionModal() {
+    setIsNewTransctionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransctionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransctionModal={handleOpenNewTransctionModal} />
       <Dashboard />
+
+      <NewTransactionModal
+        isOpen={isNewTransctionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
       <GlobalStyle />
     </>
   );
