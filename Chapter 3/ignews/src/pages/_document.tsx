13f9c2e2ebba69cd8969e76e4d25b25b9ff7ce01
Rafael Simionato <rafaasimi@hotmail.com>
2021-11-13 08:47:49 -0300

    Added initial structure of the Ignews project

--- Chapter 3/ignews/src/pages/_document.tsx
+++ Chapter 3/ignews/src/pages/_document.tsx
@@ -12,6 +12,8 @@ export default class MyDocument extends Document {
             rel="stylesheet"
           />
 
+          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+
         </Head>
         <body>
           <Main />
