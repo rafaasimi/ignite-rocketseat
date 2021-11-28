44cffd6f105bb952aaa20f9a6be8156aa7761282
Rafael Simionato <rafaasimi@hotmail.com>
2021-11-28 09:51:10 -0300

    Created payment system with Stripe and saving information with FaunaDB

--- Chapter 3/ignews/src/pages/api/auth/[...nextauth].ts
+++ Chapter 3/ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,40 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import { query as q } from "faunadb";
+
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      scope: 'read:user'
+      scope: "read:user",
     }),
   ],
-})
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          // Se não existe um usuario que bate com esse e-mail, crio um novo registro
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email))
+              )
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            // Se não, apenas retorno o usuario com o email correspondente
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
+          )
+        );
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
+});
